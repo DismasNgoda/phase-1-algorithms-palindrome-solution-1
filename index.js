60e0d9ef227b392e045b823147ff356dfc6a91f7
Dismas Ng'oda <dismas.ngoda@student.moringaschool.com>
2022-08-30 11:01:49 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function reverseWord(word) {
   // Write your algorithm here
+  /*
+  initialize a function to reverse the input
+  creates variables to store each output
+  call the reverseWord function in another function to check test performance.
+  create an if statement with expected returns
+  try out multiple cases if it matches the required palindrome set
+  */
   const newWord = word.split('');
   const reversedNewWord = newWord.reverse();
   const output = reversedNewWord.join('');
@@ -18,10 +25,16 @@ function isPalindrome(word){
 
 /* 
   Add your pseudocode here
+  initializes reverseWord function(str)  
+  const newWord, reversedNewWord, output;
+  invokes the if else statement
 */
 
 /*
   Add written explanation of your solution here
+  initializes reverseWord function(str), passing the a string as its parameter 
+  each variable stores a new outcome: split, reversed and joined outcomes.
+  the if else statements checks the words and returns bolean true or false
 */
 
 // You can run `node index.js` to view these console logs
